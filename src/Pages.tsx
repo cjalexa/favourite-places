c049ef1f7a17a105cc4287ff43bce7d1be4a39b9
Louis Freeman <louis.freeman@gmail.com>
2021-06-16 15:34:30 +0100

    Addition of props

---
+++ src/Pages.tsx
@@ -0,0 +1,16 @@
+interface PagesProps {
+    title : string
+    placename : string
+    countryname : string
+    mainimage   : string
+    link_to_rough_location : string
+    description : string
+}
+
+function Page(props: PagesProps): JSX.Element {
+    return(
+        <img src='#' alt {props.mainimage} />
+    )
+}
+
+export default Page
