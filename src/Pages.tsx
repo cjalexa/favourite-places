01e97d3cf33d713fc94f30aad580cf92179690ea
Louis Freeman <louis.freeman@gmail.com>
2021-06-16 15:52:48 +0100

    Body added

--- src/Pages.tsx
+++ src/Pages.tsx
@@ -3,13 +3,23 @@ interface PagesProps {
     placename : string
     countryname : string
     mainimage   : string
+    main_image_address : string
     link_to_rough_location : string
     description : string
 }
 
 function Page(props: PagesProps): JSX.Element {
     return(
-        <img src='#' alt ={props.mainimage} />
+        <main>
+            <div className = 'image'>
+                <img src = {props.main_image_address} alt ={props.mainimage}/>
+            </div>
+            <h1>{props.title}</h1>
+            <h2>{props.placename},{props.countryname} <a href={props.link_to_rough_location}> (map link) </a></h2>
+            <div className = 'description'>
+                {props.description}
+            </div>
+        </main>
     )
 }
 
