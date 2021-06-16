f6b82204e2e63f180df076c8c318246b51587ff7
Louis Freeman <louis.freeman@gmail.com>
2021-06-16 16:15:45 +0100

    Pages content created

---
+++ src/Page-Content.tsx
@@ -0,0 +1,46 @@
+import Pages from './Pages'
+
+function PagesContent(): JSX.Element{
+    return (
+        <main>
+            <Pages
+            title : {"Machu Picchu"}
+            placename : {"Cusco"}
+            countryname : {"Peru"}
+            mainimage   : {"Sun rising over Machu Picchu"}
+            main_image_address : {"https://www.dropbox.com/s/prxx6kl6guxaey6/2016-06-18%2007.12.10%20HDR-2.jpg?dl=0"}
+            link_to_rough_location : {"https://goo.gl/maps/9BkQALg25vHjRpVc7"}
+            description : {"Having trekked for 4 days along the Inca trail,seeing the sun rise over Machu Picchu was one of the best experiences of my life"}
+            />
+            <Pages
+            title : {""}
+            placename : {""}
+            countryname : {""}
+            mainimage   : {""}
+            main_image_address : {""}
+            link_to_rough_location : {""}
+            description : {""}
+            />
+            <Pages
+            title : {""}
+            placename : {""}
+            countryname : {""}
+            mainimage   : {""}
+            main_image_address : {""}
+            link_to_rough_location : {""}
+            description : {""}
+            />
+            <Pages
+            title : {""}
+            placename : {""}
+            countryname : {""}
+            mainimage   : {""}
+            main_image_address : {""}
+            link_to_rough_location : {""}
+            description : {""}
+            />
+    </main>
+    )
+}
+
+export default PagesContent
