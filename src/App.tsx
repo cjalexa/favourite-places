ed89a9af5497984d54215bbcc83bf4481211730d
Louis Freeman <louis.freeman@gmail.com>
2021-06-17 09:30:52 +0100

    Adding header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import Header from './Header';
 import PageContent from './Page-Content';
 import './App.css';
 
 function App() : JSX.Element{
   return (
     <>
+      <Header />
       <PageContent />
     </>
   );
---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+function Header() : JSX.Element {
+    return (
+        <div className = 'header'>
+            <h1>
+                My favourite places
+            </h1>
+        </div>
+    )
+}
+
+export default Header
